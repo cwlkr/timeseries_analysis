5023f1c1a9596088dc10eaaf1d9637e4fcb11692
Cédric Walker <35108155+cwlkr@users.noreply.github.com>
2019-03-04 14:26:40 +0100

    Update data.R

--- data.R
+++ data.R
@@ -16,21 +16,26 @@ erk.ratio.var = "erk.ratio"
 meta.grouping = "Grouping"
 
 #laod dataset
-## example single ------
+## example single ------ 
+#### Only useful if imaging.data is mounted in os
 mnt =  "/run/user/1000/gvfs/afp-volume:host=izbhelsinki,volume=imaging.data" # ubuntu
 mnt = 'y:'                                                                   # windows
+mnt = ""                                                                     # just exp path.
+
+
+####### path to Merged folder
 experiment = "Coralie/NIH3T3/siPOOLs/20190221_systIII_siPOOLs_plate1_singlePulses/20190221_090712_551/Merged"
 metaname = "20190221_NIH3T3_syst_III_siPOOL_plate1_singlePulses_I.xlsx"
-# example multi -----------
-#mnt =  "/run/user/1000/gvfs/afp-volume:host=izbhelsinki,volume=imaging.data"
-#experiment = "Coralie/NIH3T3/siPOOLs/20181213_systIII_siPOOLs_plate1_multipulses_III/20181213_174451_769/Merged"
-#metaname = "20181213_NIH3T3_syst_III_siPOOL_plate1_multiPulses.xlsx"
-# ---------
+pdf.filename = "plots.pdf" # title of pdf the plots well be rendered in 
+##########
 
-pdf.filename = "plots_multi.pdf" 
+metadatapath = paste(mnt, experiment, metaname, sep = "/") # or just the path to the metadata file
+
+
 
 path = paste(mnt, experiment, "cp.out", sep = "/") # or just the path to tCoursesSelected.csv
-metadatapath = paste(mnt, experiment, metaname, sep = "/") # or just the path to the metadata file
+metaname = "20190221_NIH3T3_syst_III_siPOOL_plate1_singlePulses_I.xlsx"
+
 
 # Read csv file 
 s.file = list.files(path = path, pattern = "tCoursesSelected.csv", recursive = FALSE, full.names = TRUE)
@@ -47,12 +52,12 @@ dt.data = dt.data[, erk.ratio := get(cyto.erk)/get(nuc.erk)]
 stim.times <- metadata %>% select(contains(stim.time.var), stim.var) %>% 
   select(contains(stim.time.var)) %>% slice(1)  %>% c(., recursive=TRUE) %>% as.numeric()
 
-
 #just to make sure that grouping and stim_treatment are ok!
 groups <- metadata %>% # group_by(.dots = meta.grouping) %>% 
-  select(stim.var, meta.grouping) %>% mutate("Grouping" = as.numeric(get(meta.grouping)))
+ select(stim.var, meta.grouping, Position, Well) %>% rename("Image_Metadata_Site" = "Position", "Metadata_Well" = "Well") %>%
+  mutate("Grouping" = as.numeric(get(meta.grouping)), Image_Metadata_Site = as.numeric(Image_Metadata_Site), Metadata_Well = as.numeric(Metadata_Well)) 
 
-dt.data = left_join(dt.data, groups)
+dt.data =   left_join(dt.data, groups)
 
 source('~/MasterProjects/timeseries_analysis/plots.R')
 
